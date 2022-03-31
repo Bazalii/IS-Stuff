74f798223f89ba8fefb201554fe8dc9f59b34c99
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-31 22:30:30 +0300

    feat: add abstract class Algorithm for different algorithms

---
+++ AppliedMath/FirstLab/FirstLab/Algorithms/Algorithm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstLab.Algorithms
+{
+    public abstract class Algorithm
+    {
+        protected FunctionDelegate Function;
+
+        protected int Accuracy;
+
+        protected double Epsilon;
+
+        protected Algorithm(FunctionDelegate function, int accuracy)
+        {
+            Function = function;
+            Accuracy = accuracy;
+            Epsilon = Math.Pow(10, -Accuracy);
+        }
+
+        public abstract void Execute(double left, double right);
+
+        protected void DrawGraph(List<double> points)
+        {
+            
+        }
+    }
+}
