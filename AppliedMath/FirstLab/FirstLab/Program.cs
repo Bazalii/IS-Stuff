47dd003bf6bb79574d9fed91e708db8796b6877e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-31 22:29:03 +0300

    feat: realise Program class

---
+++ AppliedMath/FirstLab/FirstLab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FirstLab.Algorithms.Implementations;
+
+namespace FirstLab
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var application =
+                new Application(new GoldenSearch(x => Math.Round(Math.Log(x * x, Math.E) + 1 - Math.Sin(x), 10),
+                    5));
+            application.Execute(6, 10);
+        }
+    }
+}
