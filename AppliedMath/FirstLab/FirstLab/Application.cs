03d84e3dcf56f37568ecae076fa1fd5338ceb155
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-31 22:29:40 +0300

    feat: realise Application class

---
+++ AppliedMath/FirstLab/FirstLab/Application.cs
@@ -0,0 +1,21 @@
+﻿using FirstLab.Algorithms;
+
+namespace FirstLab
+{
+    public class Application
+    {
+        private readonly Algorithm _algorithm;
+
+        public Application(Algorithm algorithm)
+        {
+            _algorithm = algorithm;
+        }
+
+        public void Execute(double left, double right)
+        {
+            _algorithm.Execute(left, right);
+        }
+    }
+    
+    public delegate double FunctionDelegate(double x);
+}
